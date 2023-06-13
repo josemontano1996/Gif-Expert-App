1221e541b1636ffd18a0c1a9e8fb6a77a9782bee
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-06-13 11:18:29 +0200

    onSubmit

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -1,7 +1,30 @@
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+
 export const GiftExpertApp = () => {
+  //nunca poner hooks en condicionales
+  const [categories, setCategories] = useState(['One Punch']);
+
+  const onAddCategory = () => {
+    setCategories(['Valorant', ...categories]);
+    /*  setCategories(categories => ['Valorant', ...categories]); */
+  };
+
   return (
     <>
+      {/* titulo */}
       <h1>GiftExpertApp</h1>
+
+      {/* Input */}
+      <AddCategory setCategories={setCategories}  />
+
+      {/* Listado de Gifs*/}
+      <ol>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
+      {/* Listado de gifs items */}
     </>
   );
 };
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+export const AddCategory = ({ setCategories }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const onInputChage = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (inputValue.trim().length <= 1) return;
+    setCategories((categories) => [inputValue, ...categories]);
+    setInputValue('');
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input type='text' placeholder='Buscar gifs' value={inputValue} onChange={onInputChage} />
+    </form>
+  );
+};
